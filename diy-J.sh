f3bea118b2005e2e58591d09411f973df0468c9a
zkquan555 <87291136+zkquan555@users.noreply.github.com>
2022-10-09 08:56:11 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -23,7 +23,7 @@ echo "RELEASE_KEY_PASSWORD=TVBoxOSC" >>$CURRENT_DIR/$DIR/gradle.properties
 #xwalk修复
 #sed -i 's/download.01.org\/crosswalk\/releases\/crosswalk\/android\/maven2/raw.githubusercontent.com\/lm317379829\/TVBoxDIY\/main/g' $CURRENT_DIR/$DIR/build.gradle
 #名称修改
-sed -i 's/TVBox/box/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
+sed -i 's/TVBox/Box/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
 #图标修改
 #cp $CURRENT_DIR/DIY/app_icon.png $CURRENT_DIR/$DIR/app/src/main/res/drawable-hdpi/app_icon.png
 #cp $CURRENT_DIR/DIY/app_icon.png $CURRENT_DIR/$DIR/app/src/main/res/drawable-xhdpi/app_icon.png
